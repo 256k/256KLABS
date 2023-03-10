32ae0f30282e882530b58b4d98d6195f0c91621d
256k <fayek.helmi@gmail.com>
2023-03-09 20:52:00 -0500

    added an automatic dark mode between 7pm and 7 am

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,13 @@
-console.log('script.js loaded. see assets/javascript/script.js');
+
+let html = document.getElementsByTagName("html")[0];
+
+let date = new Date();
+let dateHour = date.getHours();
+
+if (dateHour > 19 && dateHour < 7 ) {
+  html.className = "dark";
+  console.log("dark mode enabled");
+} else {
+  html.className = "";
+  console.log("dark mode disabled");
+}
