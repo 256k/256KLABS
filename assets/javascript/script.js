be30e8958bfb7249fe387edbe8a6dd23d1e45d42
256k <fayek.helmi@gmail.com>
2023-03-09 20:55:43 -0500

    fixed conditional logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,10 +4,10 @@ let html = document.getElementsByTagName("html")[0];
 let date = new Date();
 let dateHour = date.getHours();
 
-if (dateHour > 19 && dateHour < 7 ) {
+if (dateHour > 19 || dateHour < 7 ) {
   html.className = "dark";
   console.log("dark mode enabled");
 } else {
-  html.className = "";
+  html.className = "day";
   console.log("dark mode disabled");
 }
